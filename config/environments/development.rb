b79e5fa382a46f362582eb7a2b459367c6d8705f
Manuel-IA <manuelpc1720@gmail.com>
2020-04-24 00:12:51 -0500

    pruebas

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,4 @@
 Rails.application.configure do
-
-  config.sass.preferred_syntax = :sass
-  config.sass.syntax = :sass
-
   # Settings specified here will take precedence over those in config/application.rb.
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
